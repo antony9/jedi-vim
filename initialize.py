c5e635062857738418834e9529124a6be871b4b6
moyun.li <limoyun@foxmail.com>
2014-09-27 21:22:51 +0800

    Add virenv support
    Usage:
        1. add "let g:jedi#virtual_env_path='/path/to/go'" in vimrc
        2. enjoy
    
    TBD:
        1. if not configured, an error will be reported
        2. space in path?
        3. need well tested
    
    Note:
        1. must specified before /site-packages
        2. will import all *.egg in site-packages

--- initialize.py
+++ initialize.py
@@ -18,8 +18,18 @@ import traceback
 
 # update the sys path to include the jedi_vim script
 sys.path.insert(0, vim.eval('expand(s:script_path)'))
+
+def append_vir_env():
+    venv = vim.eval('g:jedi#virtual_env_path').split()[-1]
+    venv = os.path.abspath(venv)
+    import glob
+    eggs = glob.glob(os.path.join(venv, "*.egg"))
+    sys.path.insert(0, venv)
+    sys.path.extend(eggs)
+
 try:
     import jedi_vim
+    append_vir_env()
 except ImportError:
     vim.command('echoerr "Please install Jedi if you want to use jedi_vim."')
 sys.path.pop(1)
